5d91c1fce5f4920ffa1217f384ac132b5b090097
programmer5000-com <programmer5000@programmer5000.com>
2018-05-05 13:32:44 -0400

    added some websocket test code & start of a spec

--- public/index.js
+++ public/index.js
@@ -1,1 +1,9 @@
 console.log("hello world");
+let socket = new WebSocket("ws://localhost:8080");
+socket.onopen = () => {
+  console.log("[socket] connected");
+  socket.send(Uint8Array.from([0b11111111, 0b11111111, 0b11111111]));
+};
+socket.onmessage = (e) => {
+  console.log("[socket] recieved data", e);
+};
