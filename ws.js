6f4c5f80668af25045a8836016e3a3018be91137
max <programmer5000@programmer5000.com>
2018-05-16 20:55:03 -0400

    Allow specifying the host to bind on via $HOST

--- ws.js
+++ ws.js
@@ -2,7 +2,7 @@
 const MAX_CONNECTION_BROKEN_WAIT = 5 * 1000;
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port: process.env.PORT || 5000 });
+const wss = new WebSocket.Server({ port: process.env.PORT || 5000, host: process.env.HOST || "127.0.0.1" });
 
 const mainEmitter = require("./index.js");
 
