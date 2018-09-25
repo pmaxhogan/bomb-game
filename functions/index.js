dd7f13b9316d7e05dbe4812f1127376b86db277f
programmer5000-com <programmer5000@programmer5000.com>
2018-09-25 17:45:45 -0400

    Fix errors in the functions

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,9 @@ const functions = require("firebase-functions");
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 
+const settings = {timestampsInSnapshots: true};
+db.settings(settings);
+
 exports.setupNewUser = functions.auth.user().onCreate((user) => {
-  db.collection("users").doc(user.uid).set({username: Math.random() * Math.pow(10, 17) + ""});
+  return db.collection("users").doc(user.uid).set({username: Math.random() * Math.pow(10, 17) + ""});
 });
